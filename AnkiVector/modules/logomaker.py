d55bc0d822b3bc9ae63158b7c4dab96bf4e9c91f
im-bb <87819247+im-bb@users.noreply.github.com>
2021-10-06 12:35:53 +0530

    hmm

--- AnkiVector/modules/logomaker.py
+++ AnkiVector/modules/logomaker.py
@@ -333,7 +333,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "AnkiVectorLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By Miss_Isabella_Robot ⚡️")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Miss_Isabella_Robot ⚡️")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -371,7 +371,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "AnkiVectorLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By  Miss_Isabella_Robot⚡️")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Miss_Isabella_Robot⚡️")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -410,7 +410,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "AnkiVectorLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By Miss_Isabella_Robot ⚡️")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Miss_Isabella_Robot ⚡️")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
